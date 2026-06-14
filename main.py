344b025a6c5415bd06c7eaae7918b97c27e24491
simcha <simchacop@gmail.com>
2026-06-14 23:31:35 +0300

    feat: add members_router to app

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.book_routes import router as books_router
+from routes.member_routes import router as members_router
 
 app = FastAPI()
 
-app.include_router(books_router, prefix="/books")
+app.include_router(books_router, prefix="/books", tags=["books"])
+app.include_router(members_router, prefix="/members", tags=["members"])
