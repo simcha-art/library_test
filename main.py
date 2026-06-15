53070ac8da9d52e5f5fed9ffeee6115b4ea1db14
simcha <simchacop@gmail.com>
2026-06-15 12:46:37 +0300

    feat: add exeption handler to catch mysql errors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, exception_handlers, Request, HTTPException
 from database.db_connection import db
 from routes.book_routes import router as books_router
 from routes.member_routes import router as members_router
 from routes.report_routes import router as reports_router
 from contextlib import asynccontextmanager
+from mysql.connector import Error as MYSQL_ERROR
+from logs.logger_config import logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,6 +19,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan= lifespan)
 
+@app.exception_handlers(MYSQL_ERROR)
+def sql_errors(req: Request, e: MYSQL_ERROR):
+    logger.error(f"mysql error, request: {req.url}, exeption: {e.msg}")        
+    raise HTTPException(500, "internal error")
+
 app.include_router(books_router, prefix="/books", tags=["books"])
 app.include_router(members_router, prefix="/members", tags=["members"])
 app.include_router(reports_router, prefix ="/reports", tags=["reports"])
+
+
