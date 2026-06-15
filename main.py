4cd81b0b6515cb2a91d807ea7a4a7eec630290da
simcha <simchacop@gmail.com>
2026-06-15 10:05:16 +0300

    feat: include reports_router to app

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routes.book_routes import router as books_router
 from routes.member_routes import router as members_router
+from routes.report_routes import router as reports_router
 
 app = FastAPI()
 
 app.include_router(books_router, prefix="/books", tags=["books"])
 app.include_router(members_router, prefix="/members", tags=["members"])
+app.include_router(reports_router, prefix ="/reports", tags=["reports"])
